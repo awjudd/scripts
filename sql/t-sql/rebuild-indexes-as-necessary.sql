b55afab9d97f9a3b9e210359616c5c2fc4c14dc7
Andrew Judd <awjudd@gmail.com>
2013-04-25 22:13:14 -0400

    Adding in the script to rebuild indexes on tables as necessary

---
+++ sql/t-sql/rebuild-indexes-as-necessary.sql
@@ -0,0 +1,69 @@
+-- ==========================================================================================
+--  Name: DataLoad.RebuildIndexesAsNecessary
+--  Date: August 7, 2012
+--  Author: Andrew Judd
+--  Description: Used to go through all of the tables in your specified database and
+--               check the fragmentation levels of the indexes.  If the indexes are
+--               highly fragmented, it will rebuild it, otherwise it will just try to
+--               run a reorganize.
+-- ==========================================================================================
+CREATE PROCEDURE DataLoad.RebuildIndexesAsNecessary
+	@RebuildThreshold FLOAT = 40
+	, @FragmentationThreshold FLOAT = 10
+AS
+BEGIN
+
+	SET NOCOUNT ON
+
+	CREATE TABLE #Fragmentation 
+	(
+		  TableName NVARCHAR(200)
+		, IndexName NVARCHAR(200)
+		, FragmentationAmount DECIMAL(18,4)
+	)
+
+	-- Load all of the fragmented tables
+	INSERT  INTO #Fragmentation ( TableName, IndexName, FragmentationAmount )
+			SELECT  DISTINCT 
+					TableName = S.name + '.' + tbl.[name]
+					, IndexName = ind.name
+					, FragmentationAmount = MAX(mn.avg_fragmentation_in_percent)
+			FROM    sys.dm_db_index_physical_stats(NULL, NULL, NULL, NULL, NULL) AS mn
+			INNER JOIN sys.tables tbl ON tbl.[object_id] = mn.[object_id]
+			INNER JOIN sys.indexes ind ON ind.[object_id] = mn.[object_id]
+			INNER JOIN sys.schemas S ON tbl.schema_id = S.schema_id
+			WHERE   [database_id] = DB_ID()
+					AND mn.avg_fragmentation_in_percent > @FragmentationThreshold
+					AND ind.type_desc <> 'NONCLUSTERED COLUMNSTORE'
+					AND ind.name IS NOT NULL
+			GROUP BY S.name + '.' + tbl.[name], ind.name
+			ORDER BY MAX(mn.avg_fragmentation_in_percent) DESC
+
+	WHILE EXISTS ( SELECT TOP 1
+							TableName
+				   FROM     #Fragmentation ) 
+		BEGIN
+			DECLARE @TableName AS NVARCHAR(200)
+				   , @IndexName AS NVARCHAR(200)
+				   , @FragmentationAmount DECIMAL(18,4)
+				   , @sql VARCHAR(1000)
+			SELECT TOP 1
+					@TableName = TableName
+					, @IndexName = IndexName
+					, @FragmentationAmount = FragmentationAmount
+			FROM    #Fragmentation
+
+			SET @sql = 'ALTER INDEX ' + @IndexName + ' ON ' + @TableName + CASE WHEN @FragmentationAmount > @RebuildThreshold THEN ' REBUILD' ELSE ' REORGANIZE' END
+            
+			PRINT @sql
+			EXEC(  @sql )
+
+			DELETE  FROM #Fragmentation WHERE   TableName = @TableName AND IndexName = @IndexName
+
+			BREAK
+		END
+
+	DROP TABLE #Fragmentation
+
+
+END
